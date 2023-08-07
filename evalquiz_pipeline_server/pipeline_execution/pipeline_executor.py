9f341e0399aaa4dc695172ada4bcac0c7c64bd76
st170001 <st170001@stud.uni-stuttgart.de>
2023-08-07 22:59:43 +0200

    Black formatting

--- evalquiz_pipeline_server/pipeline_execution/pipeline_executor.py
+++ evalquiz_pipeline_server/pipeline_execution/pipeline_executor.py
@@ -1,8 +1,13 @@
 from typing import AsyncIterator
 from evalquiz_proto.shared.generated import BatchStatus, PipelineModule, PipelineStatus
 
-class PipelineExecutor():
-    
-    async def get_pipeline_status_on_change(self, pipeline_thread: str) -> AsyncIterator[PipelineStatus]:
+
+class PipelineExecutor:
+    async def get_pipeline_status_on_change(
+        self, pipeline_thread: str
+    ) -> AsyncIterator[PipelineStatus]:
         # This is just for test purposes, # Not Implemented!
-        yield PipelineStatus(None, [BatchStatus(error_message=None, pipeline_module=PipelineModule(value=0))])
+        yield PipelineStatus(
+            None,
+            [BatchStatus(error_message=None, pipeline_module=PipelineModule(value=0))],
+        )
--- evalquiz_pipeline_server/server_component.py
+++ evalquiz_pipeline_server/server_component.py
@@ -1,11 +1,14 @@
-from evalquiz_pipeline_server.pipeline_execution.pipeline_executor import PipelineExecutor
+from evalquiz_pipeline_server.pipeline_execution.pipeline_executor import (
+    PipelineExecutor,
+)
 from evalquiz_proto.shared.generated import (
     InternalConfig,
     PipelineServerBase,
     PipelineStatus,
 )
-from grpclib.server import Server
+# from grpclib.server import Server
 from typing import AsyncIterator
+
 
 class PipelineServerService(PipelineServerBase):
     """Serves endpoints for material manipulation."""
@@ -13,10 +16,12 @@ class PipelineServerService(PipelineServerBase):
     def __init__(self) -> None:
         self.pipeline_executor = PipelineExecutor()
 
-    async def iterate_config(self, internal_config: InternalConfig) -> "AsyncIterator[PipelineStatus]":
+    async def iterate_config(
+        self, internal_config: InternalConfig
+    ) -> "AsyncIterator[PipelineStatus]":
         pipeline_thread = "Test hash!"
-        pipeline_status_iterator = (
-            self.pipeline_executor.get_pipeline_status_on_change(pipeline_thread)
+        pipeline_status_iterator = self.pipeline_executor.get_pipeline_status_on_change(
+            pipeline_thread
         )
         while True:
             try:
