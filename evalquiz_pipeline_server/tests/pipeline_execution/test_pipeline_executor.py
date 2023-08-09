f89155567fd742b41b225553ee6e28748c303185
st170001 <st170001@stud.uni-stuttgart.de>
2023-08-09 14:16:49 +0200

    Indepth tests of split-merge validation

--- evalquiz_pipeline_server/tests/pipeline_execution/test_pipeline_executor.py
+++ evalquiz_pipeline_server/tests/pipeline_execution/test_pipeline_executor.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Any
+from typing import Any, Tuple
 import pytest
 from evalquiz_pipeline_server.pipeline_execution.pipeline import Pipeline
 from evalquiz_pipeline_server.pipeline_execution.pipeline_executor import (
@@ -11,19 +11,41 @@ from evalquiz_pipeline_server.pipeline_module_implementations.pipeline_module_im
 from evalquiz_proto.shared.generated import PipelineModule
 
 
+class TestPipelineModuleImplementation(PipelineModuleImplementation):
+    def run(input: Any) -> Any:
+        raise NotImplementedError()
+
+
 @pytest.fixture(scope="session")
 def pipeline_executor() -> PipelineExecutor:
     pipeline_executor = PipelineExecutor()
     return pipeline_executor
 
 
-def test_validate_implementation_for_pipeline(
-    pipeline_executor: PipelineExecutor,
-) -> None:
-    class TestPipelineModuleImplementation(PipelineModuleImplementation):
-        def run(input: Any) -> Any:
-            raise NotImplementedError()
+"""Describes test cases when implementation validation should fail and pass.
+First element: tuple of booleans: Split configurations: (a_split, b_split, c_split)
+Second element: tuple booleans: Merge configurations: (a_merge, b_merge, c_merge)
+Third element: bool: The expected outcoming validate_implementation_for_pipeline() boolean.
+"""
+split_merge_configurations = [
+    ((True, False, False), (False, False, True), True),
+    ((False, False, False), (False, False, False), True),
+    ((False, False, False), (True, False, False), False),
+    ((True, True, False), (False, False, False), False),
+    ((True, True, False), (True, False, True), True),
+]
 
+
+@pytest.mark.parametrize(
+    "split_configurations, merge_configurations, expected", split_merge_configurations
+)
+def test_validate_implementation_for_pipeline(
+    split_configurations: Tuple[bool, bool, bool],
+    merge_configurations: Tuple[bool, bool, bool],
+    expected: bool,
+) -> None:
+    (a_split, b_split, c_split) = split_configurations
+    (a_merge, b_merge, c_merge) = merge_configurations
     test_pipeline_modules = [
         PipelineModule("a", "str", "str"),
         PipelineModule("b", "str", "int"),
@@ -35,9 +57,15 @@ def test_validate_implementation_for_pipeline(
     ] = defaultdict(
         None,
         {
-            "a": TestPipelineModuleImplementation(test_pipeline_modules[0], split=True),
-            "b": TestPipelineModuleImplementation(test_pipeline_modules[0]),
-            "c": TestPipelineModuleImplementation(test_pipeline_modules[0], merge=True),
+            "a": TestPipelineModuleImplementation(
+                test_pipeline_modules[0], split=a_split, merge=a_merge
+            ),
+            "b": TestPipelineModuleImplementation(
+                test_pipeline_modules[0], split=b_split, merge=b_merge
+            ),
+            "c": TestPipelineModuleImplementation(
+                test_pipeline_modules[0], split=c_split, merge=c_merge
+            ),
         },
     )
     test_pipeline_dict: defaultdict["str", Pipeline] = defaultdict(
@@ -48,5 +76,5 @@ def test_validate_implementation_for_pipeline(
     )
     assert (
         pipeline_executor.validate_implementation_for_pipeline(test_pipeline.reference)
-        == True
+        == expected
     )
